25874161b358a4386505ebddbe068dd73f69234d
Kolya Venturi <kolya.venturi@enx3s.com>
2018-03-22 15:11:36 -0600

    Update flash

--- spec/features/user_edits_an_article_spec.rb
+++ spec/features/user_edits_an_article_spec.rb
@@ -35,7 +35,7 @@ describe 'user edits an article' do
         expect(page).to have_content 'New Title'
         expect(page).to have_content 'New Body'
 
-        expect(page).to have_content('Article New Title was updated.')
+        expect(page).to have_content("Article 'New Title' Updated!")
       end
     end
   end
