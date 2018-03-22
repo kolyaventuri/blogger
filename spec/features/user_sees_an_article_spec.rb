8cbb6e5a63dc62185b483d81533d6ec5481a736b
Kolya Venturi <kolya.venturi@enx3s.com>
2018-03-22 15:27:55 -0600

    Test that articles have comments

--- spec/features/user_sees_an_article_spec.rb
+++ spec/features/user_sees_an_article_spec.rb
@@ -5,13 +5,15 @@ describe 'user sees one article' do
     before :all do
       DatabaseCleaner.clean
     end
-    
+
     after :all do
       DatabaseCleaner.clean
     end
 
     it 'displays information for one article' do
       article = Article.create!(title: 'New Title', body: 'New Body')
+      comment1 = article.comments.create(author_name: 'Me', body: 'Commenty comments')
+      comment2 = article.comments.create(author_name: 'You', body: 'So much to say')
 
       visit articles_path
 
@@ -19,6 +21,11 @@ describe 'user sees one article' do
 
       expect(page).to have_content(article.title)
       expect(page).to have_content(article.body)
+
+      expect(page).to have_content(comment1.author_name)
+      expect(page).to have_content(comment1.body)
+      expect(page).to have_content(comment2.author_name)
+      expect(page).to have_content(comment2.body)
     end
   end
 end
