962cc9bf5167ef3d20c3588e390f7fc992a7d1a4
Kolya Venturi <kolya.venturi@enx3s.com>
2018-03-22 14:35:56 -0600

    Edit link test

---
+++ spec/features/user_edits_an_article_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe 'user edits an article' do
+  describe 'they visit the article page' do
+    before :each do
+      DatabaseCleaner.clean
+      @article1 = Article.create!(title: 'Title 1', body: 'Body 1')
+      @article2 = Article.create!(title: 'Title 2', body: 'Body 2')
+    end
+
+    after :all do
+      DatabaseCleaner.clean
+    end
+
+    context 'they click edit on the show view' do
+      it 'allows them to edit the article' do
+
+        visit article_path(@article1)
+        click_link 'Edit'
+
+        expect(current_path).to eq(articles_edit_path)
+      end
+    end
+  end
+end
